c002a8fe914ff37f964fe6cc0cb4984b84b9ddde
MitaliDogra <124426274+MitaliDogra@users.noreply.github.com>
2023-02-21 11:34:08 +0530

    Add files via upload

---
+++ A.java
@@ -0,0 +1,12 @@
+import java.io.* ;
+class A {
+ public static void main(String args[])
+ {
+ StringBuffer sb = new StringBuffer("Hello ");
+ sb.append("Java"); // now original string is changed
+ sb.delete(1,3);
+ sb.replace(1,3,"abc");
+ sb.reverse();
+ System.out.println(sb);
+ }
+}
