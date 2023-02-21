2d99177d94c0ac9b72053c987b90d22eba982741
MitaliDogra <124426274+MitaliDogra@users.noreply.github.com>
2023-02-21 22:27:48 +0530

    Update and rename Two_matrices.java to Twomatrices.java

--- Twomatrices.java
+++ Twomatrices.java
@@ -1,7 +1,7 @@
-public class Two_matrices{
+public class Twomatrices{
     public static void main(String args[]){
-       int a[][]={{1,2,3},{4,5,6},{7,8,9}};
-       int b[][]={{1,1,1},{1,1,1},{1,1,1}};
+       int a[][]={{9,8,7},{1,2,3},{6,3,4}};
+       int b[][]={{2,4,3},{7,4,2},{1,2,3}};
        int c[][]=new int[3][3];
  
        for(int i = 0;i<3;i++){
