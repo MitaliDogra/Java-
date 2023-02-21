c02377947327c974f7c5343430393742a688ce59
MitaliDogra <124426274+MitaliDogra@users.noreply.github.com>
2023-02-21 22:26:42 +0530

    Update Transpose.java

--- Transpose.java
+++ Transpose.java
@@ -1,12 +1,10 @@
 public class Transpose{  
     public static void main(String args[]){  
-    //creating a matrix  
+     
     int original[][]={{1,3,4},{2,4,3},{3,4,5}};    
         
-    //creating another matrix to store transpose of a matrix  
-    int transpose[][]=new int[3][3];  //3 rows and 3 columns  
-        
-    //Code to transpose a matrix  
+    int transpose[][]=new int[3][3];  
+         
     for(int i=0;i<3;i++){    
     for(int j=0;j<3;j++){    
     transpose[i][j]=original[j][i];  
@@ -18,13 +16,13 @@ public class Transpose{
     for(int j=0;j<3;j++){    
     System.out.print(original[i][j]+" ");    
     }    
-    System.out.println();//new line    
+    System.out.println();   
     }    
     System.out.println("Printing Matrix After Transpose:");  
     for(int i=0;i<3;i++){    
     for(int j=0;j<3;j++){    
     System.out.print(transpose[i][j]+" ");    
     }    
-    System.out.println();//new line    
+    System.out.println();  
     }    
     }}  
